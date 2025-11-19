d76af8c0e412424b8dbdefeb273f7e310ac1442d
musaautosmart-beep <musaautosmart@gmail.com>
2025-11-19 15:08:10 +0500

    ....

--- lib/custom_widgets/os_map_picker.dart
+++ lib/custom_widgets/os_map_picker.dart
@@ -103,7 +103,7 @@ class _OSMMapPickerScreenState extends State<OSMMapPickerScreen> {
             children: [
               TileLayer(
                 urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-                userAgentPackageName: 'com.example.app',
+                userAgentPackageName: 'com.petadoption.app',
               ),
               MarkerLayer(
                 markers: [
