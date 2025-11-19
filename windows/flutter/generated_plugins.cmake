06d93ca41e4c982860ecfda328aaff91a919883e
muhammadmusadev19 <muhammadmusadev19@gmail.com>
2025-11-19 04:43:20 -0800

    ....

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,18 +4,12 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   cloud_firestore
-  connectivity_plus
   file_selector_windows
   firebase_core
   geolocator_windows
-  local_auth_windows
-  permission_handler_windows
-  tflite_flutter_plus
-  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
-  tflite_flutter
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
