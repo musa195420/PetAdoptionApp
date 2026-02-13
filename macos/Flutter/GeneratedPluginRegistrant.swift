67955b569935f4f9d2c5295965988e05397dd764
Muhammad Musa <musa195420@gmail.com>
2026-02-13 08:04:45 -0800

    ...

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -16,7 +16,6 @@ import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
-import tflite_flutter_plus
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -31,6 +30,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  TfliteFlutterPlusPlugin.register(with: registry.registrar(forPlugin: "TfliteFlutterPlusPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -10,7 +10,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   geolocator_windows
   local_auth_windows
   permission_handler_windows
-  tflite_flutter_plus
   url_launcher_windows
 )
 
